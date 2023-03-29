15286fe0d90eb8a59047d1f36504a5c1ea1c37be
Sonu das <sonudasrd999@gmail.com>
2023-03-29 21:21:27 +0530

    Update testform.php
    added $server global variable how it works

--- php/testform.php
+++ php/testform.php
@@ -1,8 +1,17 @@
 <?php
 
 echo "<pre>";
-echo json_encode($_REQUEST);
-echo "</pre>"
+echo print_r($_REQUEST);
+echo "</pre>";
 
 
+
+echo "<pre>";
+echo print_r($_SERVER);
+echo "</pre>";
+
+
+echo $_SERVER['SERVER_ADMIN'];
+echo "<br>";
+echo $_REQUEST['name'];
 ?>
